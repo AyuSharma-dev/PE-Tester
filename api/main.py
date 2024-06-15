6042d5bc0d30ab1e10977ad079ef6583f0dc46be
Ayush Sharma <ayusharmadev@gmail.com>
2024-06-15 22:41:23 +0530

    Changed Streaming Library
    Replace AIOStream with Salesforce Pub/Sub library for Python.

--- api/main.py
+++ api/main.py
@@ -10,6 +10,16 @@ import requests
 from urllib.parse import urlparse
 import utility.constants as cons
 import nest_asyncio
+import grpc
+import requests
+import threading
+import io
+import pubsub_api_pb2 as pb2
+import pubsub_api_pb2_grpc as pb2_grpc
+import avro.schema
+import avro.io
+import time
+import certifi
 
 
 app = Flask(__name__)
@@ -21,10 +31,31 @@ app.config.from_object(__name__)
 Session(app)
 nest_asyncio.apply()
 cache = {}
+semaphore = threading.Semaphore(1)
+latest_replay_id = None
 
 # socketio = SocketIO(app, logger=True, engineio_logger=True, async_mode="threading") #Initiating SocketIo
 # socketio.init_app(app, cors_allowed_origins="*")
 
+## Method does fetch request for platform event
+def fetchReqStream(topic):
+    while True:
+        semaphore.acquire()
+        yield pb2.FetchRequest(
+            topic_name = topic,
+            replay_preset = pb2.ReplayPreset.LATEST,
+            num_requested = 1)
+
+## Method decodes the incoming Event response
+def decode(schema, payload):
+    schema = avro.schema.parse(schema)
+    buf = io.BytesIO(payload)
+    decoder = avro.io.BinaryDecoder(buf)
+    reader = avro.io.DatumReader(schema)
+    ret = reader.read(decoder)
+    return ret
+
+## Method gets called for authentication
 @app.route('/', methods=['POST', 'GET'])
 def home():
     authUrl = 'client_id='+cons.CLIENT_ID
@@ -44,13 +75,26 @@ def oauthCallback():
     # global domain
     session['authDetails'] = getAccessToke( request.args.get('code') )
     if( session.get('authDetails') != None ):
+        print('access_token', session.get('authDetails').get('access_token') )
+        print('all info', session.get('authDetails') )
+        session['access_token'] = session.get('authDetails').get('access_token')
+        getOrgId()
         session['refresh_token'] = session.get('authDetails').get('refresh_token')
         session['org_url'] = urlparse(session.get('authDetails').get('instance_url')).netloc
+        session['instance_url'] = session.get('authDetails').get('instance_url')
+        print('org url--',session['instance_url'])
     else:
         return render_template( 'error.html' )
     
     return render_template( 'redirectingTemp.html', urlToOpen='/subscribeToPE' )
 
+## Method decodes Org ID from URL
+def getOrgId():
+    orgUrl = session.get('authDetails').get('id')
+    orgId = orgUrl[ orgUrl.index( '/id/' )+4: len(orgUrl) ]
+    orgId = orgId[ 0 : orgId.index('/') ]
+    print('ordId--'+orgId)
+    session['org_id'] = orgId
 
 #Method stores the Org Domain
 @app.route('/getOrgType', methods=['POST', 'GET'])
@@ -62,89 +106,93 @@ def getOrgType():
     session.modified = True
     return render_template( 'EventSubscription.html' )
 
-
+## Method maintains ClientKey
 @app.route('/getReceivedDetails', methods=['POST', 'GET'])
 def getReceivedDetails():
     clientKey = request.args.get('clientKey')
-    return dumps(cache[clientKey])
+    if( clientKey ):
+        return dumps(cache[clientKey])
 
 
-#Renders the EventSubscription template.
+## Renders the EventSubscription template.
 @app.route('/subscribeToPE', methods=['POST', 'GET'])
 def getPEDetails():
     return render_template("EventSubscription.html", org_name=session.get('org_url') )
 
-
+## Method gets called on Subscribe button and Subscribe the Event
 @app.route('/eventDetails', methods=['POST'])
 def handleEventDetails():
-    return stream_events2( request.json['eventName'], request.json['eventType'], request.json['clientKey'] )
+    return stream_events3( request.json['eventName'], request.json['eventType'], request.json['clientKey'] )
 
-def stream_events2( eventName, eventType, clientKey ):
+def stream_events3( eventName, eventType, clientKey ):
     try:
         session['eventName'] = eventName
         session['channelType'] = eventType
         session['clientKey'] = clientKey
+        print('here1-->',session['eventName'], session['channelType'])
         if( session.get('loop') is None ):
             cache[clientKey] = []
-            session['loop'] = asyncio.new_event_loop()
-            session['loop'].run_until_complete(perform_message( session.get('eventName'), session.get('channelType'), session.get('clientKey') ))
-        else:
-            send_fut = asyncio.run_coroutine_threadsafe( perform_message( session.get('eventName'), session.get('channelType') ), session.get('loop'))
+       
+        handleEvents()
         return dumps({'success':True, 'clientKey':session['clientKey']}), 200, {'ContentType':'application/json'} 
     except Exception as e:
+        print('here4-->',e)
         return dumps({'success':False, 'clientKey':session['clientKey'], 'message': 'Sorry something went wrong!!'}), 400, {'ContentType':'application/json'} 
 
+#portNum = 7443
+def handleEvents():
 
-#Method perfrom Auth for the Event Subscription and Subscribes to the Event
-async def perform_message( eventName, channelType, clientKey ):
-    listeningTo = ( 'Push Topics' if channelType == 'topic' else 'Platform Events' )
-    eventPath = "/"+channelType+"/"+eventName
-    isSandbox = False
-    if( session.get('unSubEventsOnly') == True and session.get('client') is not None ):
-        session['unSubEventsOnly'] = False
-        await session.get('client').unsubscribe(session.get('eventPath'))
-        session['client'] = None
-        session['eventPath'] = None
+    clientKey = session['clientKey']
+    exStub = session.get('stub')
+    print('exStub--',exStub)
+    with open(certifi.where(), 'rb') as f:
+        creds = grpc.ssl_channel_credentials(f.read())
+    
+    portNum = '7443'
+    with grpc.secure_channel('api.pubsub.salesforce.com:'+portNum, creds) as channel:
 
-    else:
-        session['unSubEventsOnly'] = False
-        if( session.get('client') is None ):
+        # if( session.get('unSubEventsOnly') == True and session.get('client') is not None ):
+        #     session['unSubEventsOnly'] = False
+        #     await session.get('client').unsubscribe(session.get('eventPath'))
+        #     session['client'] = None
+        #     session['eventPath'] = None
 
-            auth = RefreshTokenAuthenticator(
-                    consumer_key=cons.CLIENT_ID,
-                    consumer_secret=cons.CLIENT_SECRET,
-                    refresh_token=session.get('refresh_token'),
-                    sandbox=cons.SANDBOX_DOMAIN == session.get('orgDomain')
-                )
-            
-            client = Client(auth) #Authorizing the client with Refresh Token.
+        sessionid = session.get('access_token')
+        instanceurl = session.get('instance_url')
+        tenantid = session.get('org_id');
+        authmetadata = (('accesstoken', sessionid),
+        ('instanceurl', instanceurl),
+        ('tenantid', tenantid))
 
-            session['client'] = client
-            await session.get('client').open()
-            if( session.get('eventPath') is not None and session.get('eventPath') != eventPath ):
-                await session.get('client').unsubscribe(session.get('eventPath'))
-        
-        if( session.get('eventPath') is None or (session.get('eventPath') != eventPath) ):
-            
-            if( session.get('eventPath') is not None and session.get('eventPath') != eventPath ):
-                await session.get('client').unsubscribe(session.get('eventPath'))
-
-            session['eventPath'] = eventPath
-            await session.get('client').subscribe(eventPath) #Subscribing to Event
-
-            # listen for incoming messages
-            async for message in session.get('client'):
-                data = message["data"]
-                payload = None
-                
-                if 'payload' in data:
-                    payload = data["payload"]
-                    cache[clientKey].append(payload) 
-                else:
-                    payload = data['sobject']
-                    cache[clientKey].append(payload) 
+        if( exStub is None ):
+            stub = pb2_grpc.PubSubStub(channel)
+            session['stub'] = stub
         else:
-            print('inside else')
+            stub = exStub
+
+        mysubtopic = "/"+session.get('channelType')+"/"+session.get('eventName')
+        #mysubtopic = "/event/Notification__e"
+        print('Subscribing to ' + mysubtopic)
+        substream = stub.Subscribe(fetchReqStream(mysubtopic),
+                metadata=authmetadata)
+        for event in substream:
+            if( event is not None ):
+                if event.events:
+                    semaphore.release()
+                print("Number of events received: ", len(event.events))
+                if( len(event.events) > 0 ):
+                    payloadbytes = event.events[0].event.payload
+                    schemaid = event.events[0].event.schema_id
+                    schema = stub.GetSchema(
+                            pb2.SchemaRequest(schema_id=schemaid),
+                            metadata=authmetadata).schema_json
+                    decoded = decode(schema, payloadbytes)
+                    cache[clientKey].append(decoded) 
+                    print("Got an event!", decoded)
+            else:
+                print("[", time.strftime('%b %d, %Y %l:%M%p %Z'),
+                "] The subscription is active.")
+            latest_replay_id = event.latest_replay_id
 
 
 #Method logs out user
@@ -177,5 +225,4 @@ def getAccessToke( code ):
 ##Running the app
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    # socketio.run(app)
+    socketio.run(app)
